2aae11491821c61d34bb4ef823699d84d68b8bc2
DmitryMikheev <mikheevdmitry94@gmail.com>
2014-08-30 01:28:27 +0400

    Revert "created a few blocks"
    This reverts commit 1669adff4deba61c4b8f46461fd947dfa905c447.

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $('body').click(function(e) {
-    if($(this).hasClass('_design')) {
-      $(this).removeClass('_design');
-    } else {
-      $(this).addClass('_design');
-    }
-  });
-});
